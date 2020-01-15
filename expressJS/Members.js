76a09b10eb9750ace830b072c43a5be248685cc4
DESKTOP-MSG9PH7\Shivam <contact@shivam.me>
2020-01-15 10:17:03 +0530

    Started expressJS

---
+++ expressJS/Members.js
@@ -0,0 +1,19 @@
+const members = [
+  {
+    id: 0,
+    name: "Shivam Gupta",
+    phone: "928838823"
+  },
+  {
+    id: 1,
+    name: "Sundaram Gupta",
+    phone: "928833423"
+  },
+  {
+    id: 2,
+    name: "Renu Gupta",
+    phone: "924566623"
+  }
+];
+
+module.exports = members;
---
+++ expressJS/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+const members = require("./Members");
+
+const app = express();
+
+const logger = (req, res, next) => {
+  console.log(` ${req.protocol}://${req.get("host")}${req.originalUrl} `);
+  next();
+};
+
+// Init Middleware
+app.use(logger);
+
+// Get all members
+app.get("/api/members", (req, res) => {
+  res.json(members);
+});
+
+// Set a static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
